0bfcf21eab45d3ef7102c470ddf433ebf8eef313
joaomoschetta <joaopedromschneider123@gmail.com>
2021-07-28 04:35:50 -0300

    Removed alternative api

--- src/services/api.ts
+++ src/services/api.ts
@@ -6,10 +6,7 @@ export default async function getWeatherData(lat: string, lon: string) {
   
   const weatherApiPath = `http://api.weatherapi.com/v1/forecast.json?key=${process.env.REACT_APP_WEATHER_API_KEY}&q=${lat},${lon}&days=3&aqi=no&alerts=no
   `;
-  
-  const tomorrowIoPath = `https://api.tomorrow.io/v4/timelines?location=${lat},${lon}&fields=temperature,weatherCode&timesteps=1h&units=metric&apikey=${process.env.REACT_APP_TOMORROW_IO_KEY}`;
 
-  // If cant reach first api, will try to use de second one
   const apiData : any = await axios.get(weatherApiPath)
     .then(response => {
       if (response && response.status === 200) {
@@ -17,14 +14,6 @@ export default async function getWeatherData(lat: string, lon: string) {
       }
     })
     .catch(err => console.log(err))
-    ||
-    await axios.get(tomorrowIoPath)
-      .then(response => {
-        if(response && response.status === 200) {
-          return response;
-        }
-      })
-      .catch(err => console.log(err));
 
   const weatherData = unifyApiData(apiData);
   console.log(weatherData); // TEMP
---
+++ src/services/unifyApiData.ts
@@ -0,0 +1,113 @@
+/*
+  This functionality was made to get easier in the if
+  future new alternative apis been add, this must put
+  all apis in one pattern 
+*/
+
+import { AxiosResponse } from 'axios';
+
+
+// Main weather data
+interface IForecastCondition {
+  text: string | any;
+  icon: string | any;
+}
+
+interface IForecastHour {
+  time: string;
+  temperatureInCelsius: number;
+  condition: IForecastCondition;
+}
+
+interface IForecast {
+  date: string;
+  minCelsiusTemperature: number;
+  maxCelsiusTemperature: number;
+  condition: IForecastCondition;
+  hour: IForecastHour[];
+};
+
+interface IWeatherData {
+  now: {
+    temperatureInCelsius: number;
+    condition: IForecastCondition;
+  }
+  today: {
+    minCelsiusTemperature: number;
+    maxCelsiusTemperature: number;
+  }
+  forecast: IForecast[];
+};
+
+// API: weatherApi
+interface IWeatherApiDayForecastHour {
+  time: string;
+  temp_c: number;
+  condition: IForecastCondition;
+}
+
+interface IWeatherApiDayForecast {
+  date: string;
+  day: {
+    mintemp_c: number;
+    maxtemp_c: number;
+    condition: IForecastCondition;
+  };
+  hour: IWeatherApiDayForecastHour[];
+}
+
+function weatherApiUnify(rawData : AxiosResponse) {
+  const data = rawData.data;
+  const currentCondition = data.current.condition;
+  const forecast = data.forecast;
+  const todayForecast = forecast.forecastday[0].day;
+
+  const dailyForecast = forecast.forecastday.map((value : IWeatherApiDayForecast) => {
+    const dailyForecastHour = value.hour.map((value) : IForecastHour => {
+      return {
+        time: value.time,
+        temperatureInCelsius: value.temp_c,
+        condition: value.condition,
+      } as IForecastHour
+    });
+
+    return {
+      date: value.date,
+      minCelsiusTemperature: value.day.mintemp_c,
+      maxCelsiusTemperature: value.day.maxtemp_c,
+      condition: value.day.condition,
+      hour: dailyForecastHour,
+    } as IForecast;
+  })
+
+  const weatherData : IWeatherData = {
+    now: {
+      temperatureInCelsius: Math.round(data.current.temp_c),
+      condition: {
+        text: currentCondition.text,
+        icon: currentCondition.icon,
+      }
+    },
+    today: {
+      minCelsiusTemperature: Math.round(todayForecast.mintemp_c),
+      maxCelsiusTemperature: Math.round(todayForecast.maxtemp_c),
+    },
+    forecast: dailyForecast
+  };
+
+  return weatherData;
+}
+
+function unifyApiData(rawData: AxiosResponse<object>) {
+  const url = String(rawData.config.url);
+
+  // if new alternative api been added, add new regex test and case for switch
+  const weatherApiRegex = /weatherapi/;
+
+  switch (true) {
+    case weatherApiRegex.test(url):
+      return weatherApiUnify(rawData);
+  }
+};
+
+export { unifyApiData };
