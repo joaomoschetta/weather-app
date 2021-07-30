3498a069fbf1c5f58dadf3e50534bee10439a762
joaomoschetta <joaopedromschneider123@gmail.com>
2021-07-30 18:37:08 -0300

    Current weather component + style

--- src/components/CurrentWeather.tsx
+++ src/components/CurrentWeather.tsx
@@ -1,12 +1,38 @@
 import { IWeatherDataNow, IWeatherDataToday} from '../services/unifyApiData';
 
+import '../styles/current-weather.scss';
+
 export interface IProps {
   now: IWeatherDataNow;
   today: IWeatherDataToday;
 }
 
 export function CurrentWeather(props : IProps) {
+  const now = props.now;
+
   return (
-    <div></div>
+    <div className="current-weather">
+      <h1 data-testid="current-temperature">
+        {`${props.now.temperatureInCelsius}°C`}
+      </h1>
+      <div className="down-row">
+        <img 
+          data-testid="current-condition"
+          src={now.condition.icon}
+          alt={now.condition.text}
+        />
+
+        <div className="vertical-bar"></div>
+
+        <div className="day-min-max-temperature">
+          <p data-testid="min-temperature">
+            {`${props.today.maxCelsiusTemperature}°`}
+          </p>
+          <p data-testid="max-temperature">
+            {`${props.today.minCelsiusTemperature}°`}
+          </p>
+        </div>
+      </div>
+    </div>
   )
 }
