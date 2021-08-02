1052789c4eb0466498b6dcf63d244eb9e1363598
joaomoschetta <joaopedromschneider123@gmail.com>
2021-08-02 10:37:15 -0300

    Added fun to get week day name

--- src/components/DayForecast.tsx
+++ src/components/DayForecast.tsx
@@ -8,13 +8,20 @@ export interface IProps {
 export function DayForecast(props : IProps) {
   const forecast = props.weatherData.forecast;
 
+  function getWeekDayName(date: string) {
+    const dayDate = new Date(date);
+    const weekDayName = String(dayDate).slice(0, 3);
+
+    return weekDayName;
+  }
+
   return (
     <div className="day-forecast">
       <ul>
         {forecast.map((dayForecast, index) => {
           return (
             <li key={index}>
-              <p data-testid="abbreviation">{dayForecast.date.slice(0, 3)}</p>
+              <p data-testid="abbreviation">{getWeekDayName(dayForecast.date)}</p>
               <img
                 data-testid="day-condition"
                 src={dayForecast.condition.icon}
