970ec9b1c346f23c5e3344a98699fb57c4831324
joaomoschetta <joaopedromschneider123@gmail.com>
2021-08-03 20:57:22 -0300

    Added landing page + style

---
+++ src/pages/LandingPage.tsx
@@ -0,0 +1,59 @@
+import React, { useState, useEffect } from "react";
+
+import { CurrentWeather } from "components/CurrentWeather";
+import { DayForecast } from "components/DayForecast";
+import { NextHoursForecast } from "components/NextHoursForecast";
+import { Footer } from "components/Footer";
+
+import getWeatherData from '../services/api';
+
+import '../styles/landing-page.scss';
+
+import { IWeatherData } from '../services/unifyApiData';
+
+export function LandingPage() {
+  const [weatherData, setWeatherData] = useState<IWeatherData>();
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(
+      async position => {
+        const coordinates = position.coords;
+        const latitude = String(coordinates.latitude);
+        const longitude = String(coordinates.longitude);
+
+        setWeatherData(await getWeatherData(latitude, longitude));
+      },
+      error => {
+        console.log(error);
+      }
+    );
+  }, []);
+
+  return (
+    <div className="landing-page">
+      {weatherData ? (
+        <>
+          <div className="top-main-content">
+            <CurrentWeather
+              now={weatherData.now}
+              today={weatherData.today}
+            />
+            <DayForecast
+              weatherData={weatherData}
+            />
+          </div>
+          <NextHoursForecast
+            weatherData={weatherData}
+          />
+          <Footer />
+        </>
+      ) : (
+        // If don't return data, load.
+        <div className="loader-allow-location">
+          <div className="loader"></div>
+          <h2>Allow location</h2>
+        </div>
+      )}
+    </div>
+  )
+}
