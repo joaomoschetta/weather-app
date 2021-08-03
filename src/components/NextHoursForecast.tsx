56ee14b195cdb32f83aa7a4e0b91d0332c92a745
joaomoschetta <joaopedromschneider123@gmail.com>
2021-08-03 18:58:07 -0300

    Added NextHours component + style

--- src/components/NextHoursForecast.tsx
+++ src/components/NextHoursForecast.tsx
@@ -2,6 +2,8 @@ import React from "react";
 
 import { IWeatherData } from "../services/unifyApiData";
 
+import '../styles/next-hours-forecast.scss';
+
 export interface IProps {
   weatherData: IWeatherData;
 }
