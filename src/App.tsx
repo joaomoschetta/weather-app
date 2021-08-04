f24c4bbbd7e2d964139dd5eed8d00ebdb37d409a
joaomoschetta <joaopedromschneider123@gmail.com>
2021-08-04 14:23:28 -0300

    App router-dom set

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,19 @@
 import React from 'react';
-import './services/api';
+import { BrowserRouter, Route } from 'react-router-dom';
+
+import { LandingPage } from 'pages/LandingPage';
+
+import './styles/global.scss';
 
 function App() {
   return (
     <div className="App">
-      
+      <BrowserRouter>
+        <Route
+          path='/'
+          component={LandingPage}
+        />
+      </BrowserRouter>
     </div>
   );
 }
