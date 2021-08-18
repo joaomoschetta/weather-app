b9b9f4fc1e6f69b5076789fcc5ece509a8f6b948
joaomoschetta <joaopedromschneider123@gmail.com>
2021-08-17 21:22:48 -0300

    Deploy fix: set API URL to https

--- src/services/api.ts
+++ src/services/api.ts
@@ -4,7 +4,7 @@ import { unifyApiData } from './unifyApiData';
 
 export default async function getWeatherData(lat: string, lon: string) {
   
-  const weatherApiPath = `http://api.weatherapi.com/v1/forecast.json?key=${process.env.REACT_APP_WEATHER_API_KEY}&q=${lat},${lon}&days=3&aqi=no&alerts=no
+  const weatherApiPath = `https://api.weatherapi.com/v1/forecast.json?key=${process.env.REACT_APP_WEATHER_API_KEY}&q=${lat},${lon}&days=3&aqi=no&alerts=no
   `;
 
   const apiData : any = await axios.get(weatherApiPath)
